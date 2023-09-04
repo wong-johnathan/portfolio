de189ce795157967b2b6d99ec6dc495b19a6717f
wong-johnathan <77264935+wong-johnathan@users.noreply.github.com>
2023-09-04 22:55:34 +0800

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { navItems } from "./NavLinks";
+
+const Footer = () => {
+  return (
+    <footer className='bg-white shadow dark:bg-gray-800'>
+      <div className='w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between'>
+        <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+          © Copyright 2023. Made by{" "}
+          <span className='underline font-bold'>
+            Johnathan Wong
+          </span>
+        </span>
+        <ul className='flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 gap-4'>
+          {navItems().map((item) => (
+            <Link
+              className='hover:underline hover:text-gray-200 transition-all duration-300'
+              key={`${item.text}-${item.link}`}
+              href={item.link}
+            >
+              <li>{item.text}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
